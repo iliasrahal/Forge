82d9fdd2e191ac7e354e2a2cabf8a3dd7686c41c
ProjectForge-code <momocomoriano93@gmail.com>
2026-08-25 16:19:08 +0200

    debug prisma 3

--- forge-main/forge-main/src/lib/prisma.ts
+++ forge-main/forge-main/src/lib/prisma.ts
@@ -1,36 +1,59 @@
 import { PrismaPg } from "@prisma/adapter-pg";
+
 import { PrismaClient } from "@/src/generated/prisma/client";
+
 
 const databaseUrl = process.env.DATABASE_URL;
 
+
 if (!databaseUrl) {
+
   throw new Error(
-    "La variable DATABASE_URL est absente du fichier .env."
+    "La variable DATABASE_URL est absente du fichier .env.",
   );
+
 }
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
 
 
 const adapter =
   new PrismaPg({
-    connectionString: databaseUrl,
+
+    connectionString:
+      `${databaseUrl}?connection_limit=5`,
+
   });
+
+
+
+const globalForPrisma =
+  globalThis as unknown as {
+    prisma?: PrismaClient;
+  };
+
 
 
 export const prisma =
+
   globalForPrisma.prisma ??
+
   new PrismaClient({
+
     adapter,
+
     log:
       process.env.NODE_ENV === "development"
+
         ? ["error", "warn"]
+
         : ["error"],
+
   });
 
 
+
 if (process.env.NODE_ENV !== "production") {
+
   globalForPrisma.prisma = prisma;
+
 }
