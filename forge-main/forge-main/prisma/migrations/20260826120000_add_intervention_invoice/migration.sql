b7f5a0a940c1b4f14f70a55fe60ac683e5ad31f5
ProjectForge-code <momocomoriano93@gmail.com>
2026-08-26 15:07:04 +0200

    bouton facture debus 6

--- forge-main/forge-main/prisma/migrations/20260826120000_add_intervention_invoice/migration.sql
+++ forge-main/forge-main/prisma/migrations/20260826120000_add_intervention_invoice/migration.sql
@@ -1,7 +1,5 @@
 ALTER TABLE "Invoice" ADD COLUMN "interventionId" TEXT;
 
-CREATE UNIQUE INDEX "Invoice_interventionId_key" ON "Invoice"("interventionId");
-
 CREATE INDEX "Invoice_interventionId_idx" ON "Invoice"("interventionId");
 
 ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_interventionId_fkey" FOREIGN KEY ("interventionId") REFERENCES "Intervention"("id") ON DELETE SET NULL ON UPDATE CASCADE;
