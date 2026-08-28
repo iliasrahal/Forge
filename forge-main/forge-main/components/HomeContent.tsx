655db8cdccaa3f43f8bae56e48d892632e07dab8
ProjectForge-code <momocomoriano93@gmail.com>
2026-08-28 11:26:51 +0200

    update just retour

--- forge-main/forge-main/components/HomeContent.tsx
+++ forge-main/forge-main/components/HomeContent.tsx
@@ -497,7 +497,7 @@ export default function HomeContent({
             className="mt-6 rounded-2xl bg-blue-600 px-6 py-3 font-semibold text-white transition hover:bg-blue-700"
           >
 
-            Retour à l'accueil
+            Retour 
 
           </button>
 
